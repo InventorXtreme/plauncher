ec09dd789d5fe90790bdc394f14565541fe24bba
InventorXtreme <inventorxtreme@gmail.com>
2024-02-24 01:37:23 -0500

    added stuff

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/InventorXtreme/plauncher
 
 go 1.20
 
-require github.com/andygrunwald/vdf v1.1.0 // indirect
+require (
+	github.com/andygrunwald/vdf v1.1.0
+	github.com/spf13/pflag v1.0.5
+)
--- plauncher.go
+++ plauncher.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"errors"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/andygrunwald/vdf"
+	flag "github.com/spf13/pflag"
 )
+
+var DirectoryNotEmpty = errors.New("Directory Not Empty")
+var ProtonNotFound = errors.New("Proton Not Found in Steam Libraries")
+
+const colorRed = "\033[0;31m"
+const colorGreen = "\033[0;32m"
+const colorNone = "\033[0m"
 
 type game struct {
 	id                string
@@ -31,6 +42,7 @@ type protonrunable struct {
 	steamcompatdatapath          string //i.e.: /home/inventorx/Hardspace.Shipbreaker.v1.3.0/Hardspace.Shipbreaker.v1.3.0
 	protonpath                   string //i.e.: /home/inventorx/.steam/steam/steamapps/common/Proton - Experimental/proton
 	exepath                      string //i.e.: /home/inventorx/Hardspace.Shipbreaker.v1.3.0/Hardspace.Shipbreaker.v1.3.0/Shipbreaker.exe
+	workingdir                   string //working dir of windows executable
 	exeargs                      string //i.e.: -no-splash
 }
 
@@ -42,26 +54,17 @@ type proton struct {
 }
 
 func (p protonrunable) ConvertToCall() string {
-	return fmt.Sprintf("STEAM_COMPAT_CLIENT_INSTALL_PATH=\"%s\" STEAM_COMPAT_DATA_PATH=\"%s\" %s run \"%s\" %s", p.steamcompatclientinstallpath, p.steamcompatdatapath, p.protonpath, p.exepath, p.exeargs)
+	return fmt.Sprintf(" cd \"$(dirname \"$(realpath -- \"%s\")\")\" &&      STEAM_COMPAT_CLIENT_INSTALL_PATH=\"%s\" STEAM_COMPAT_DATA_PATH=\"%s\" \"%s/proton\" run \"%s\" %s", p.workingdir, p.steamcompatclientinstallpath, p.steamcompatdatapath, p.protonpath, p.exepath, p.exeargs)
 }
 
 func MakeProtonRunable(s steaminstance, compatpath string, protonu proton, exepath string, exeargs string) protonrunable {
 	//envargs done
 	steamcompatclientinstallpath := s.install
-	steamcompatdatapath := ""
-	if compatpath == "" {
-		b, _ := os.UserHomeDir()
-		path := b + "/.plauncher/default"
-		err := os.MkdirAll(path, os.ModePerm)
-		if err != nil {
-			panic(err)
-		}
-		steamcompatdatapath = path
-	}
+	steamcompatdatapath := compatpath
 	protonpath := protonu.installfoldername
 	//exepath done
 	//exeargs done
-	p := protonrunable{steamcompatclientinstallpath: steamcompatclientinstallpath, steamcompatdatapath: steamcompatdatapath, protonpath: protonpath, exepath: exepath, exeargs: exeargs}
+	p := protonrunable{steamcompatclientinstallpath: steamcompatclientinstallpath, steamcompatdatapath: steamcompatdatapath, protonpath: protonpath, exepath: exepath, exeargs: exeargs, workingdir: filepath.Dir(exepath)}
 	return p
 }
 
@@ -171,17 +174,59 @@ func (g game) GetRunCommand(steam steaminstance, exefile string) string {
 	return Steamclientstring
 }
 
+func AskYesNo(prompt string) bool {
+	fmt.Fprintf(os.Stdout, "%s [%sY%s/%sN%s]", prompt, colorGreen, colorNone, colorRed, colorNone)
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.Trim(text, "\n")
+	if text == "y" || text == "Y" {
+		return true
+	}
+	return false
+}
+
+func MakePrefixIfNotExist(path string) error {
+	_, err := os.Stat(path)
+	if err != nil { // directory does not exist
+		if AskYesNo("CompatData path not found, create path: \"" + path + "\"?") {
+			os.MkdirAll(path, os.FileMode(0777))
+		} else {
+			panic(errors.New("raa"))
+		}
+
+	}
+	dircontlist, err := os.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(dircontlist) > 0 {
+		good := false
+		for _, item := range dircontlist {
+			if item.Name() == "pfx.lock" {
+				good = true
+				break
+			}
+		}
+
+		if good == false {
+			return DirectoryNotEmpty
+		}
+	}
+	return nil
+
+}
+
 func GetSteamGameList(s steaminstance) ([]game, []proton) {
 	var gamelist []game
 	var protonlist []proton
-	fmt.Println("test")
 	dirname, err := os.UserHomeDir()
 	if err != nil {
 		panic(err)
 	}
 	fpath := dirname + "/.steam/steam/steamapps/libraryfolders.vdf"
 	fpath = s.install + "/steamapps/libraryfolders.vdf"
-	fmt.Println(fpath)
+	//fmt.Println(fpath)
 	f, err := os.Open(fpath)
 	if err != nil {
 		panic(err)
@@ -212,7 +257,7 @@ func GetSteamGameList(s steaminstance) ([]game, []proton) {
 				if err.Error() != "FileRead Err" {
 					panic(err)
 				} else {
-					fmt.Println("Missing Library @ " + location + " @ Lookup of " + gameid)
+					//fmt.Println("Missing Library @ " + location + " @ Lookup of " + gameid)
 				}
 			}
 			compatpath := location + "/steamapps/compatdata/" + string(gameid)
@@ -231,7 +276,7 @@ func GetSteamGameList(s steaminstance) ([]game, []proton) {
 				if err.Error() != "FileRead Err" {
 					panic(err)
 				} else {
-					fmt.Println("Missing Library @ " + location + " @ Lookup of " + gameid)
+					//fmt.Println("Missing Library @ " + location + " @ Lookup of " + gameid)
 				}
 
 			}
@@ -245,28 +290,91 @@ func GetSteamGameList(s steaminstance) ([]game, []proton) {
 
 // func GetListOfProtons
 
-func main() {
-	b, _ := os.UserHomeDir()
-	i := steaminstance{install: (b + "/.steam/steam/")}
-	a, p := GetSteamGameList(i)
-
-	//fmt.Println(a)
-	BetterPrintGameList(a)
-	BetterPrintProtonList(p)
-	protonid := 0
-	for id, k := range p {
-		if k.name == "Proton Experimental" {
-			protonid = id
+func GetIndexOfSelectedProtonFromId(prlist []proton, id int) (int, error) {
+	for idv, k := range prlist {
+		if k.id == strconv.Itoa(id) {
+			return idv, nil
 		}
+
 	}
-	e := exec.Command("bash", "-c", "echo $sneed")
-	var stdBuffer bytes.Buffer
-	mw := io.MultiWriter(os.Stdout, &stdBuffer)
-	e.Env = os.Environ()
-	e.Stdout = mw
-	e.Stderr = mw
-	e.Run()
-	ex := MakeProtonRunable(i, "", p[protonid], "~/h.exe", "")
-	fmt.Println(ex)
-	fmt.Println(ex.ConvertToCall())
+	return -1, ProtonNotFound
+}
+
+func HelpText() {
+	helptext := `Plauncher is a simple command line tool designed to make running non-Steam executables through Proton as simple as using Wine.
+Options:
+-h, --help: 		Disply this help text
+-c, --compat	    Path to Proton Compatdata Folder
+-p, --proton		SteamID of selected Proton (defaults to Proton Experimental)
+ _, --steampath     Path to steam instance
+-o, --printcommand  Print command instead of running it
+`
+	fmt.Println(helptext)
+
+}
+
+func main() {
+	//setup default values
+
+	uhomedir, _ := os.UserHomeDir()
+	defaultprotoncompatdata := uhomedir + "/plauncherprefix"
+	defaultsteaminstancepath := uhomedir + "/.steam/steam/"
+	defaultprotonid := 1493710
+	//setup flags
+	var protoncompatdata = "neverrep"
+	var steaminstancepath = "neverrep"
+	var protonid = 0
+	var helpmode bool = false
+	var printcommand bool = false
+	flag.StringVarP(&protoncompatdata, "compat", "c", defaultprotoncompatdata, "Path to Proton prefix")
+	flag.StringVar(&steaminstancepath, "steampath", defaultsteaminstancepath, "Path to Steam instance")
+	flag.IntVarP(&protonid, "proton", "p", defaultprotonid, "SteamID of selected Proton (Defualt is Proton Experimental)")
+	flag.BoolVarP(&helpmode, "help", "h", false, "Display Help")
+	flag.BoolVarP(&printcommand, "printcommand", "o", false, "Print the commnand instead of running it")
+	//Get flags
+
+	flag.Parse()
+	remainingargs := flag.Args()
+	if len(remainingargs) == 0 {
+		helpmode = true
+		fmt.Println("Error, no Windows executable supplied")
+	}
+
+	if helpmode {
+		HelpText()
+		return
+	}
+	err := MakePrefixIfNotExist(protoncompatdata)
+	if err != nil {
+		panic(err)
+	}
+
+	mainsteaminstance := steaminstance{install: steaminstancepath}
+	a, p := GetSteamGameList(mainsteaminstance)
+
+	protonindex, err := GetIndexOfSelectedProtonFromId(p, protonid)
+	if err != nil {
+		panic(err)
+	}
+
+	a = a // we dont want to get rid of this, otherwise a is unused (golang momment)
+
+	//BetterPrintGameList(a)
+	//BetterPrintProtonList(p)
+
+	ex := MakeProtonRunable(mainsteaminstance, protoncompatdata, p[protonindex], remainingargs[0], "")
+	call := ex.ConvertToCall()
+
+	if printcommand {
+		fmt.Println(ex)
+		fmt.Println(call)
+	} else {
+		e := exec.Command("bash", "-c", call)
+		var stdBuffer bytes.Buffer
+		mw := io.MultiWriter(os.Stdout, &stdBuffer)
+		e.Env = os.Environ()
+		e.Stdout = mw
+		e.Stderr = mw
+		e.Run()
+	}
 }
